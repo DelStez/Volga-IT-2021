73e39d8023a97c54cc73e25ca10619b0aa45e177
RdrStels <kandakova0002@gmail.com>
2021-10-02 13:53:05 +0500

    hmmmm

--- HtmlFileParser/Program.cs
+++ HtmlFileParser/Program.cs
@@ -6,27 +6,62 @@ namespace HtmlFileParser
 {
     internal class Program
     {
+        public static string path = string.Empty;
         public static ParserStart parserstart;
         public static void Main(string[] args)
         {
-            string input = Console.ReadLine();
-            string path = ParseCommand(input);
-            path = input;
-            if (path != null)
+            StartProgram();
+        }
+
+        public static void StartProgram()
+        {
+            try
             {
-                parserstart = new ParserStart(path);
-                parserstart.showTextContent();
+                Console.Write("Path to html-file: ");
+                string input = Console.ReadLine();
+                if ((path = ParseCommand(input)) != null)
+                {
+                    parserstart = new ParserStart(path);
+                    parserstart.showTextContent();
+                }
+                Console.WriteLine("Press any key for exit...");
+                Console.ReadLine();
             }
-            Console.WriteLine("f");
-            Console.ReadLine();
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("The file does not exist or the path is not correct. You can try again or exit.");
+                string input = string.Empty;
+                bool cTrue = false;
+                do
+                {
+                    Console.Write("Continue?(Y/N)");
+                    input = Console.ReadLine();
+                    switch (input)
+                    {
+                        case "N":
+                        case "n":
+                            return;
+                        case "Y":
+                        case "y":
+                            cTrue = true;
+                            break;
+                        default:
+                            Console.WriteLine("Unknown answer");
+                            break;
+                    }
+                } while (!cTrue);
+                StartProgram();
+            }
         }
 
         private static string ParseCommand(string temp)
         {
             var inputArr = temp.Split(' ').ToArray();
+            
             foreach (var v in inputArr)
             {
-                if (!string.IsNullOrEmpty(v) && v.IndexOfAny(Path.GetInvalidPathChars()) >= 0)
+                FileAttributes attr = File.GetAttributes(v);
+                if (File.Exists(v))
                 {
                     return v;
                 }
